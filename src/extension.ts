ec17db763276a8968c9542823b96716b86b9a9e1
Jonathan Shapiro <shapiroj18@gmail.com>
2021-08-02 22:39:46 -0400

    line counting working when explicit command is invoked)

--- src/extension.ts
+++ src/extension.ts
@@ -10,16 +10,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('line-counter.linecounter', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+
 		const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
-		statusBarItem.text = '$(pulse) lines selected'
-		statusBarItem.color = '#2CC8A5'
-		statusBarItem.show();
+		updateStatusBarItem(statusBarItem)
 
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function updateStatusBarItem(statusBarItem: vscode.StatusBarItem): void {
+	const n = getNumberofLinesSelected(vscode.window.activeTextEditor);
+	if (n > 0) {
+		statusBarItem.text = `$(pulse) ${ n } lines selected`
+		statusBarItem.color = '#2CC8A5';
+		statusBarItem.show();
+	} else {
+		statusBarItem.hide();
+	}
+}
+
+function getNumberofLinesSelected(editor: vscode.TextEditor | undefined): number {
+	let lines = 0;
+	if (editor) {
+		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
+	}
+	return lines;
 }
 
 // this method is called when your extension is deactivated
