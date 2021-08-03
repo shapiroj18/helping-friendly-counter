f8b0c1c1df1255ebaac896f4a773a12099b54be6
Jonathan Shapiro <shapiroj18@gmail.com>
2021-08-02 22:13:06 -0400

    extension giving status bar status with explicit command

--- src/extension.ts
+++ src/extension.ts
@@ -6,17 +6,17 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "line-counter" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('line-counter.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('line-counter.linecounter', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from line-counter!');
+		const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+		statusBarItem.text = '$(pulse) lines selected'
+		statusBarItem.color = '#2CC8A5'
+		statusBarItem.show();
+
 	});
 
 	context.subscriptions.push(disposable);
